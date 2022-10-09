3a8da01ecaf7d93f9999ab27cb4b8dec8a10443f
MishaHitko <mishagitko1997@gmail.com>
2022-10-09 13:28:12 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const firstNumber = +prompt('Enter the first number');
+const secondNumber = +prompt('Enter the second number');
+const thirdNumber = +prompt('Enter the third number');
+const result = "Arithmetic mean of numbers: " + (firstNumber + secondNumber + thirdNumber) / 3;
+alert(result);
