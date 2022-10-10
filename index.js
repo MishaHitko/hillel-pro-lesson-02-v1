caa8964cf6d77bc50a05ef0ba987a74f254327a5
MishaHitko <mishagitko1997@gmail.com>
2022-10-10 13:43:26 +0300

    home work 2

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-const firstNumber = +prompt('Enter the first number');
-const secondNumber = +prompt('Enter the second number');
-const thirdNumber = +prompt('Enter the third number');
-const arithmeticMean = (firstNumber + secondNumber + thirdNumber) / 3;
-const result = "Arithmetic mean of numbers: " + arithmeticMean;
-alert(result);
+let yearOfBirth = +prompt('Привіт користувач, який у тебе рік народження?');
+let city = prompt('У якому місті ти живеш?');
+let sport = prompt('Який твій улюблений вид спорту?'); 
+
+if (yearOfBirth === null || yearOfBirth === 0) {
+	yearOfBirth = 'Ви не вказали свій рік народження.'
+} else {
+	yearOfBirth = 'Тобі ' + (2022 - yearOfBirth) + ' років.';
+}
+
+if (city === 'Київ') {
+	city = 'Ти живеш у столиці України.';
+} else if (city === 'Вашингтон') {
+	city = 'Ти живеш у столиці США.';
+} else if (city === 'Лондон') {
+	city = 'Ти живеш у столиці Великобританії.';
+} else if (city === null || city === '') {
+	city = 'Шкода, що ви не захотіли вводити своє місто.'
+} else {
+	city = 'Ти живеш у місті ' + city + '.';
+}
+
+if (sport === 'Футбол') {
+	sport = 'Ти хочешь стати Кріштіану Роналду?';
+} else if (sport === 'Баскетбол') {
+	sport = 'Ти хочешь стати Майклом Джорданом?';
+} else if (sport === 'Бокс') {
+	sport = 'Ти хочешь стати Олександром Усиком?';
+} else if (sport === null || sport === '') {
+	sport = 'Шкода, що ви не захотіли вводити свій улюбленний вид спорту.'
+} else {
+	sport = 'Тобі подобається ' + sport + '.';
+}
+
+alert(yearOfBirth + city + sport);
